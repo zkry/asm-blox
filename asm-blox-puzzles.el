adead194f258ce8f283887318bac71dc1eaa87b1
Zachary Romero <zacromero@posteo.net>
2022-10-31 18:48:43 -0700

    fix puzzle label

--- asm-blox-puzzles.el
+++ asm-blox-puzzles.el
@@ -684,7 +684,7 @@ If B>A then send B to R, 0 to L. If A=B send 0 to L and R.")))
      (list (asm-blox--cell-sink-create :row 3
                                        :col 1
                                        :expected-data output-g
-                                       :name "P")
+                                       :name "G")
            (asm-blox--cell-sink-create :row 3
                                        :col 2
                                        :expected-data output-e
